52e20b9ae7deade8fdb06da8bb90a46a5dc8c841
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2023-02-20 11:11:49 +0500

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -152,12 +152,11 @@ class HomeController extends Controller
             }
             $to_name = 'Admin';
             $to_email = 'usama1517a@gmail.com';
-            $articles = ['asdasd','Article 2','Article 3'];
-            $data = array('name'=>"Admin", "body" => "Recieved One Boking");
+            $data = array('name'=>"Admin", "body" => "A test mail");
             Mail::send('mails.order-mail', $data, function($message) use ($to_name, $to_email) {
             $message->to($to_email, $to_name)
-            ->subject('Recieved One Booking...');
-            $message->from('xtremebooking@webexert.us','Recieved Booking');
+            ->subject('Laravel Test Mail');
+            $message->from('xtremebooking@webexert.us','Test Mail');
             });
             return redirect()->back()->with('success','Cogratulation..! Booking Successfully');
         }
